203fc71237feee564ff0bf12e41ea9932f5da626
Askeladden0219 <131897328+Askeladden0219@users.noreply.github.com>
2023-10-11 13:51:29 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const fastaFileInput = document.getElementById("fastaFile");
+    const processButton = document.getElementById("processButton");
+    const resultDiv = document.getElementById("result");
+
+    processButton.addEventListener("click", function () {
+        
+    });
+});
