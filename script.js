5f7b3a19cc718b6bb490248b59dc8db91ef92415
Askeladden0219 <131897328+Askeladden0219@users.noreply.github.com>
2023-10-11 13:54:29 +0800

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener("DOMContentLoaded", function () {
-    const fastaFileInput = document.getElementById("fastaFile");
-    const processButton = document.getElementById("processButton");
-    const resultDiv = document.getElementById("result");
-
-    processButton.addEventListener("click", function () {
-        
-    });
-});
